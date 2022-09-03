2bfc87e6fdc6d834d3477ce4e41bfbf635bfaf06
y_cx <y_cx@winning.com.cn>
2022-09-03 11:55:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <NavHeader></NavHeader>
+    <transition name="fade" mode="out-in">
+      <keep-alive>
+        <router-view/>
+      </keep-alive>
+    </transition>
+
+  </div>
+</template>
+
+<script>
+import NavHeader from '@/components/NavHeader'
+
+export default {
+  name: 'App',
+  components: { NavHeader }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.fade-enter-active, .fade-leave-active{
+ transition: all .3s;
+}
+.fade-enter, .fade-leave-to{
+ opacity: 0;
+}
+
+</style>
---
+++ src/components/joePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="joe-page">
+    {{test}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'joePage',
+  data () {
+    return {
+      test: 'this is a demo by joe'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
