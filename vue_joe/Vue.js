5a0d85db38c727be83c41b3828b71ae6ebe78c0f
y_cx <y_cx@winning.com.cn>
2022-10-04 18:00:15 +0800

    feat: 实现data属性的选项合并策略和初始化

--- vue_joe/Vue.js
+++ vue_joe/Vue.js
@@ -1,4 +1,3 @@
-const { set } = require("vue/types/umd");
 
 (function (global,factory){
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
@@ -29,7 +28,6 @@ const { set } = require("vue/types/umd");
     ];
 
 
-
     var config = {
       optionsMergeStrategy: Object.create(null)
     }
@@ -42,21 +40,37 @@ const { set } = require("vue/types/umd");
     }
     const strats = config.optionsMergeStrategy
     
-    
     	//自定义策略处理
 	strats.data = function(parentVal, childVal, vm) {
-    // todo 如何理解这里的入参： vm
 		//组件的基本原理
 		//聚焦到vm   是根实例 还是组件
 		if (!vm) { // 组件
       if(childVal && typeof childVal !== "function"){
 				console.error("data选项应该为函数 返回组件中每个实例的值")
 			}
+      return mergeData(parentVal, childVal)
       
 		} else {
-
+      return mergeData(parentVal, childVal, vm)
     }
 	}
+
+  function mergeData(parentVal, childVal, vm){
+    if (!vm) {
+			//1: 子组件中的parentVal childVal 都应该是函数
+			/*
+			会遇到的情况:
+			1: parentVal === undefined   return childVal
+			2: childVal === undefined   return parentVal
+			3: parentVal ===  function(){}  childVal ===  function(){}  mergeData  把两者的返回值对象合并成一个
+			*/
+			// todo 有空去看这部分的 vue源码
+		} else {
+			return function mergedInstanceDataFn() {
+				return typeof childVal === 'function' ? childVal.call(vm, vm) : childVal;
+			}
+		}
+  }
 
 
   	//所有钩子函数的自定义策略  parentVal === undefined   childVal === function(){}   [function(){}]
@@ -83,8 +97,6 @@ const { set } = require("vue/types/umd");
         childVal
     };
 
-
-
     function mergeOptions(parent,child,vm){
       const options = {}
       for(k in parent){
@@ -97,38 +109,91 @@ const { set } = require("vue/types/umd");
       }
 
       function mergeField(key){
-        console.log('mergeField key:',key);
+        // console.log('mergeField key:',key);
         const strat = strats[key] || defaultStrats
-        options[key] = strat(parent.key,child.key,vm, key)
+        options[key] = strat(parent[key],child[key],vm, key)
       }
 
       return options
-
     }
     // todo 修改到这里了！
     
     function callHook(vm,hookName){
-      console.log('vm:',vm);
       const hookHandlers = vm.$options[hookName]
       hookHandlers.forEach((hook)=>{
         hook.call(vm)
       })
     }
 
+    Vue.options = Object.create(null); //Vue.options = {}
+
     function initMixin(Vue) {
       Vue.prototype._init = function(options) {
         var vm = this;
-        console.log(Vue.options)
         //选项合并
         vm.$options = mergeOptions(Vue.options, options || {}, vm);
         const beforeCreateHook = vm.$options['beforeCreate']
-        beforeCreateHook && typeof beforeCreateHook === 'function' && callHook(vm,'beforeCreate')       
+        beforeCreateHook && typeof beforeCreateHook === 'function' && callHook(vm,'beforeCreate')
+        initState(vm)       
       }
     }
 
+    function initState(vm){
+      if(vm.options.data){
+        initData(vm)
+      } else {
+        // observe(vm._data = {},true)
+      }
+    }
 
+    function isPlainObject(obj){
+      toString.call(obj) === '[object, Object]'
+    }
 
-        	//config
+    function isReserved(str) {
+      var c = (str + '').charCodeAt(0);   //获取Unicode 编码  0-65535
+      return c === 0x24 || c === 0x5F     // 十六进制的Unicode 编码  $ === 0x24  _ === 0x5F
+    }
+  
+    function initData(vm){
+      var data = vm.$options.data; //  函数  mergedInstanceDataFn
+      data = vm._data = typeof data === 'function' ? data(vm) : data
+      if (!isPlainObject(data)) {
+        data = {};
+        console.error(
+          'data functions should return an object:\n' +
+          'https://vuejs.org/v2/guide/components.html#data-Must-Be-a-Function',
+          vm
+        );
+      }
+      const props = vm.$options.props
+      const methods = vm.$options.methods
+      const {_data} = vm
+      for(let k in _data){
+        if(props && hasOwn(props,k)){
+          console.error('props 中已存在 ' + k+'属性')
+        }
+        if(methods && hasOwn(methods,k)){
+          console.error('methods 中已存在 ' + k+'属性')
+        }
+        else if (!isReserved(k)) {   //$  _
+          //数据代理的时候 是否有不合法的属性
+          proxy(vm, "_data", k);
+        }
+      }
+    }
+
+    function proxy(vm,str,key){
+      Object.defineProperty(vm,key,{
+        get(){
+          return vm[str][key]
+        },
+        set(newVal){
+          vm[str][key] = newVal
+        }
+      })
+    }
+
 	function initGlobalAPI(Vue) {
 		var configDef = {};
 		configDef.get = function() {
@@ -149,18 +214,9 @@ const { set } = require("vue/types/umd");
     this._init(option);
   }
 
-  Vue.options = Object.create(null); //Vue.options = {}
-    ASSET_TYPES.forEach((type)=>{
-      Vue.options[type + 's'] = Object.create(null); //Vue.options.components
-    })
-
-
-
-    console.log('Vue.options::',Vue.options);
-
-
-
-
+  ASSET_TYPES.forEach((type)=>{
+    Vue.options[type + 's'] = Object.create(null); //Vue.options.components
+  })
 
 
     function initExtend(Vue) {
@@ -194,7 +250,6 @@ const { set } = require("vue/types/umd");
         ASSET_TYPES.forEach(function(type) {
           Sub[type] = Super[type];
         });
-        console.log(1111)
         //组件在初始化 mergeOptions  选项的合并 => 规范的检测  => 策略的处理
         Sub.options = mergeOptions(
           Super.options,    //Vue.options
